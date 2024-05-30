6177974454338fb57a2ce2420394171427bfab63
mariedevoll <marie.devoll@gmail.com>
2024-05-29 20:50:46 -0400

    copying and pasting the text i had from my first attempt at this challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Nav from "./components/NavTab";
+import Footer from "./components/Footer";
+import Page from "./components/Page";
 
 function App() {
+    const [pages] = useState([
+        {
+            name: "About",
+        }, {
+            name: "Portfolio",
+        }, {
+            name: "Resume",
+        }, {
+            name: "Contact",
+        }
+    ]);
+
+    const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+        <Header>
+            <Nav pages={pages} setCurrentPage={setCurrentPage} currentPage={currentPage}/>
+        </Header>
+        <main>
+            <Page currentPage={currentPage}/>
+        </main>
+        <Footer/>
     </div>
   );
 }
