d36db61c8ad1498788a356a15407895a03404c8f
mariedevoll <marie.devoll@gmail.com>
2024-06-04 17:37:55 -0400

    removed selfie to tty and deploy

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import "../../Images/selfie.jpeg";
+//import "../../Images/selfie.jpeg";
 
 function Header({children}) {
     return (
         <header className="header">
-            <section>
+            {/* <section>
                 <img className="selfie" src='Images/selfie.jpeg' alt='selfie'></img>
-            </section>
+            </section> */}
             <h1>
                 Hi, my name is Marie DeVoll
             </h1>
