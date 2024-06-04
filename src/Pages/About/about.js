75284d0a3c323c3a33ba3c6740ea8c1c32906f9e
mariedevoll <marie.devoll@gmail.com>
2024-06-04 18:15:28 -0400

    got my images to load

--- src/Pages/About/about.js
+++ src/Pages/About/about.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "./about.css"
+import selfie from "../../Images/selfie.jpeg"
 
 function AboutPage() {
-    return <section className="about-section">
+    const photo = [
+        {
+            title: "selfie",
+            image: selfie
+        },
+    ];
+
+    return (
+    <section className="about-section">
         <h1>About Me</h1>
-        <img src="public/Images/selfie.png" alt="selfie" className="selfie"></img>
+        <div className="about-selfie">
+            <img src={photo[0].image} alt={photo[0].title} className='selfie'></img>
+        </div>
         <h2>Software Engineer, Retail Expert</h2>
         <p className="centered">
             My name is Marie DeVoll and I am currently enrolled in a full stack web development coding course through UC Berkeley. 
@@ -13,6 +24,6 @@ function AboutPage() {
         </p>
         <p className="centered">On this page, visitors can see the projects I have worked on over the last 12 weeks</p>
     </section>
-}
+);}
 
 export default AboutPage;
--- src/Pages/Resume/resume.js
+++ src/Pages/Resume/resume.js
@@ -1,13 +1,22 @@
-import "../../Images/resume.png"
+import React from 'react';
+import './resume.css';
+import smartie from "../../Images/resume.png"
 
-function resume() {
-    
+function resume() { 
+  const resume = [
+    {
+      title: "resume",
+      image: smartie
+    },
+  ];
 
-  return <div>
-            <section>
-                <img className="resume" src='Images/resume.png' alt='resume'></img>
-            </section>    
-          </div>
+  return (
+    <section className='portfolio-section'>
+    <h2>My updated Resume:</h2>
+          <div className='centered-image'>
+              <img src={resume[0].image} alt={resume[0].title} className='resume-image'></img>
+            </div>
+    </section>
+  );
 }
-
 export default resume;
